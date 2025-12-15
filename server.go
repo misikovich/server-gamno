803268a2bf0ca79934d1a6c0d4ef7b9d154ab110
misikovichdev <misikovich.dev@gmail.com>
2025-12-15 02:23:08 +0100

    added env.go

--- server.go
+++ server.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go3/env"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	godotenv.Load()
+	fmt.Println(env.HOST.GetValue())
 }
