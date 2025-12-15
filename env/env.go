3105acb150a52c3a93296a31666e5808d4d84af2
misikovichdev <misikovich.dev@gmail.com>
2025-12-15 02:37:13 +0100

    removed env.go from gitignore, moved .env to root dir

---
+++ env/env.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type EnvKey string
+
+func (key EnvKey) Get() string {
+	return os.Getenv(string(key))
+}
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+const (
+	Host    EnvKey = "HOST"
+	Port    EnvKey = "PORT"
+	APIHost EnvKey = "API_HOST"
+	APIPort EnvKey = "API_PORT"
+)
